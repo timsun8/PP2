d432de4a16e0c2789f22f60185375202baecd8ac
timsun8 <t_nurlanov@kbtu.kz>
2024-01-20 17:48:59 +0600

    lab1

---
+++ Lab1/1.py
@@ -0,0 +1,28 @@
+# TOPIC - SYNTAX
+print("Hello World")
+
+if 5 > 2:
+    print("YES")
+# TOPIC - COMMENTS
+# This is a comment
+
+"""This is a comment
+written in 
+more than just one line"""
+# TOPIC - VARIABLES
+carname = "Volvo"
+
+x = 50
+
+x = 5
+y = 10
+print(x+y)
+
+x = 5
+y = 10
+z = x + y
+print(z)
+
+x, y, z = "Orange", "Banana", "Cherry"
+
+
