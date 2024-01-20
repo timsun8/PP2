3738c640c465d4e05c3564e6b5049f6ad5cdb99c
timsun8 <t_nurlanov@kbtu.kz>
2024-01-21 00:45:13 +0600

    lab1

--- Lab1/1.py
+++ Lab1/1.py
@@ -25,4 +25,75 @@ print(z)
 
 x, y, z = "Orange", "Banana", "Cherry"
 
+x = y = z = "Orange"
+
+def myfunc():
+    global x
+    x = "fantastic"
+# TOPIC - DATA TYPES
+x = 5
+print(type(x))
+# answer is <class 'int'>
+
+x = "Hello World"
+print(type(x))
+# answer is <class 'str'>
+
+x = 20.5
+print(type(x))
+# answer is <class 'float'>
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+# answer is <class 'list'>
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+# answer is <class 'tuple'>
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+# answer is <class 'dict'>
+
+x = True
+print(type(x))
+# answer is <class 'bool'>
+# TOPIC - NUMBERS
+x = 5
+x = float(x)
+
+x = 5.5
+x = int(x)
+
+x = 5
+x = complex(x)
+# TOPIC - STRINGS 
+x = "Hello World"
+print(len(x))
+
+txt = "Hello World"
+x = txt[0]
+
+txt = "Hello World"
+x = txt[2:5]
+
+txt = " Hello World "
+x = txt.strip()
+
+txt = "Hello World"
+txt = txt.upper()
+
+txt = "Hello World"
+txt = txt.lower()
+
+txt = "Hello World"
+txt = txt.replace("H", "J")
+
+age = 36
+txt = "My name is John, and I am {}"
+print(txt.format(age))
+
+
+
+
 
